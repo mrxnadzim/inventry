d14ee2d2142fccc974ccf2d5438e0d74120c985c
Radlyx25 <nadzimghaz@gmail.com>
2025-07-20 17:59:31 +0800

    Added frontend url

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-  origin: [`http://localhost:5173`], 
+  origin: [`http://localhost:5173`, 'https://inventry-flame.vercel.app'], 
   methods: ['GET', 'POST', 'PATCH', 'DELETE'],
   credentials: true,
 };
